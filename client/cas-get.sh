a4b842b69f397b6260b224f7cc3ca7a56b44c85d
farinajf <farinajf@gmail.com>
2017-09-24 13:57:34 +0200

    cas get

--- client/cas-get.sh
+++ client/cas-get.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
-#Usage: as-get.sh {url} {username} {password}
+#Usage: cas-get.sh {url} {username} {password}
 
 DEST="$1"
+USERNAME="$2"
+PASSWORD="$3"
+
 ENCODED_DEST=`echo "$DEST" | perl -p -e 's/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg' | sed 's/%2E/./g' | sed 's/%0A//g'`
 
-CAS_HOSTNAME=wso2
-
-USERNAME=admin
-PASSWORD=temporal
+CAS_HOST_PORT=wso2:8443
 
 COOKIE_JAR=.cookieJar
 HEADER_DUMP_DEST=.headers
 rm $COOKIE_JAR
 rm $HEADER_DUMP_DEST
 
-#Visit CAS and get a login form. This includes a unique ID for the form, which we will store in CAS_ID and attach to our form submission. jsessionid cookie will be set here
-CAS_ID=`curl -s -k -c $COOKIE_JAR https://$CAS_HOSTNAME:8443/cas/login?service=$ENCODED_DEST | grep name=.lt | sed 's/.*value..//' | sed 's/\".*//'`
+#GET login form
+curl -s -k -c $COOKIE_JAR https://$CAS_HOST_PORT/cas/login?service=$ENCODED_DEST > login.txt
 
-#Submit the login form, using the cookies saved in the cookie jar and the form submission ID just extracted. We keep the headers from this request as the return value should be a 302 including a "ticket" param which we'll need in the next request
-curl -v -k -X POST --data "username=$USERNAME&password=$PASSWORD&lt=$CAS_ID&_eventId=submit" -i -b $COOKIE_JAR -c $COOKIE_JAR https://$CAS_HOSTNAME:8443/cas/login?service=$ENCODED_DEST -D $HEADER_DUMP_DEST -o /dev/null
+H_LT=`cat login.txt | grep name=.lt | sed 's/.*value..//' | sed 's/\".*//'`
+H_EXECUTION=`cat login.txt | grep name=.execution | sed 's/.*value..//' | sed 's/\".*//'`
+H_EVENTID=`cat login.txt | grep name=._eventId | sed 's/.*value..//' | sed 's/\".*//'`
 
+echo H_LT       : $H_LT
+echo H_EXECUTION: $H_EXECUTION
+echo H_EVENTID  : $H_EVENTID
+
+
+#Submit the login form.
+#We keep the headers from this request as the return value should be a 302 including a "ticket" param which we'll need in the next request
+curl -v -k -X POST --data "username=$USERNAME&password=$PASSWORD&lt=$H_LT&execution=$H_EXECUTION&_eventId=$H_EVENTID" -i -b $COOKIE_JAR -c $COOKIE_JAR \
+         https://$CAS_HOST_PORT/cas/login?service=$ENCODED_DEST -D $HEADER_DUMP_DEST -o /dev/null
+
+TGC=`cat $HEADER_DUMP_DEST | grep TGC= | sed 's/Set-Cookie: TGC=//' | sed 's/;Path.*//'`
+TICKET=`cat $HEADER_DUMP_DEST | grep Location | grep ticket=ST | sed 's/.*ticket.//'`
+
+echo "*-------------------------------------------------------*"
+echo "*                                                       *"
+echo \* TGC   : $TGC
+echo "*                                                       *"
+echo \* TICKET: $TICKET
+echo "*                                                       *"
+echo "*-------------------------------------------------------*"
 
